e9d9fdec6536ddb9c998b6cf0072e6cc998835c3
yourjin <155814455+yourjinKR@users.noreply.github.com>
2026-08-24 23:46:16 +0900

    refactor: 프리셋 이미지 삭제시 기본 프로필로 변경하도록 수정

--- src/main/java/matchuri/backend/domain/member/repository/MemberProfileImageRepository.java
+++ src/main/java/matchuri/backend/domain/member/repository/MemberProfileImageRepository.java
@@ -3,10 +3,21 @@ package matchuri.backend.domain.member.repository;
 import java.util.Optional;
 import matchuri.backend.domain.member.entity.MemberProfileImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberProfileImageRepository extends JpaRepository<MemberProfileImage, Long> {
 
     Optional<MemberProfileImage> findByMemberId(Long memberId);
 
     boolean existsByMemberId(Long memberId);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("""
+    update MemberProfileImage mp
+        set mp.imageAsset.id = :updateAssetId
+            where mp.imageAsset.id = :deletedAssetId
+    """)
+    int updateToDefault(@Param("deletedAssetId") Long deletedAssetId, @Param("updateAssetId") Long updateAssetId);
 }
